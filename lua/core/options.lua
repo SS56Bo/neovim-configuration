c1e36a087d6f6f60f795bb3209823f2d58f4feeb
SS56Bo <sohamsarkar560@gmail.com>
2025-07-03 23:13:44 +0530

    Updated

---
+++ lua/core/options.lua
@@ -0,0 +1,5 @@
+vim.wo.number = true  -- Enables Line number --
+vim.o.relativenumber = false  -- Enables Relative Line Number
+vim.o.wrap = false -- Displays line as one long line (default: true)
+vim.o.clipboard = '' -- Sync clipboard between Neovim and Linux
+vim.o.autoindent = true -- Enables Auto-indent (default: true)
