c63b9b6588bd5df57c60262639033d9f6ecb5eb4
SS56Bo <sohamsarkar560@gmail.com>
2025-07-13 11:20:54 +0530

    Keymaps update

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,7 +9,23 @@ vim.keymap.set({'n', 'v'}, '<Space>', '<Nop>', {silent = true})
 local opts = {noremap = true, silent = true}
 
 -- SAVE FILE USING CTRL + S
-vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
+vim.keymap.set('n', '<C-s>', '<Esc><cmd> w <CR>', opts)
 
 -- SAVE FILE WITHOUT AUTO-FORMATTING
 vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
+
+-- VERTICAL SCROLL AND CENTER
+vim.keymap.set('n', '<C-w>', '<C-d>zz', opts)
+vim.keymap.set('n', '<C-s>', '<C-u>zz', opts)
+
+-- FIND AND CENTER
+vim.keymap.set('n', 'n', 'nzzzv', opts)
+vim.keymap.set('n', 'N', 'Nzzzv', opts)
+
+-- RESIZE WITH ARROWS
+vim.keymap.set('n', '<Up>', 'resize -2<CR>', opts)
+vim.keymap.set('n', '<Down>', 'resize +2<CR>', opts)
+vim.keymap.set('n', '<Left>', 'vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<Right>', 'vertical resize +2<CR>', opts)
+
+-- BUFFER
