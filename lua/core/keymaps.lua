3bd85982150bb844fbb9e3009cc356f8c225d5a3
SS56Bo <sohamsarkar345@gmail.com>
2025-07-14 18:40:27 +0530

    Keymaps update

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,4 +1,4 @@
--- Set Leader Key
+- Set Leader Key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -15,8 +15,8 @@ vim.keymap.set('n', '<C-s>', '<Esc><cmd> w <CR>', opts)
 vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
 
 -- VERTICAL SCROLL AND CENTER
-vim.keymap.set('n', '<C-w>', '<C-d>zz', opts)
-vim.keymap.set('n', '<C-s>', '<C-u>zz', opts)
+vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
+vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
 
 -- FIND AND CENTER
 vim.keymap.set('n', 'n', 'nzzzv', opts)
@@ -31,4 +31,23 @@ vim.keymap.set('n', '<Right>', 'vertical resize +2<CR>', opts)
 -- BUFFER
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
 vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
-vim.keymap.set('n', '<leader>X', ':Bdelete!<CR>', opts)
+vim.keymap.set('n', '<leader>X', ':Bdelete!<CR>', opts)  -- Close Buffer
+vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- New Buffer
+
+-- Window Management
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts)  -- Split window vertically
+vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- Split window horizontally
+vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- make split equal width
+vim.keymap.set('n', '<leader>xs>', ':close<CR>', opts) -- Close current split window
+
+-- Navigate between Splits
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
+
+
+
+
+
+
