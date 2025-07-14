4d6f6c85173952b7021429672b14e4600a9bbf5a
SS56Bo <sohamsarkar560@gmail.com>
2025-07-14 19:00:06 +0530

    Keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -14,6 +14,9 @@ vim.keymap.set('n', '<C-s>', '<Esc><cmd> w <CR>', opts)
 -- SAVE FILE WITHOUT AUTO-FORMATTING
 vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
 
+-- QUIT NEOVIM
+vim.keymap.set('n', '<C-q>', '<Esc><cmd> q <CR>', opts)
+
 -- VERTICAL SCROLL AND CENTER
 vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
 vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
@@ -38,7 +41,7 @@ vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- New Buffer
 vim.keymap.set('n', '<leader>v', '<C-w>v', opts)  -- Split window vertically
 vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- Split window horizontally
 vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- make split equal width
-vim.keymap.set('n', '<leader>xs>', ':close<CR>', opts) -- Close current split window
+vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- Close current split window
 
 -- Navigate between Splits
 vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
@@ -46,7 +49,14 @@ vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
 vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
 vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
 
+-- TABS
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
 
+-- delete single character without copying into register
+vim.keymap.set('n', 'x', '"_x', opts)
 
 
 
