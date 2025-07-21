86012298e2fd8101c264390e04c202b7ffa8a3e4
SS56Bo <sohamsarkar345@gmail.com>
2025-07-21 17:19:42 +0530

    Lua lsp loaded

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,19 +1,42 @@
 return {
     'neovim/nvim-lspconfig',
     dependencies = {
-        { 'mason-org/mason.nvim', config = true},
-        'mason-org/mason-lspconfig.nvim',
-        'WhoIsSethDaniel/mason-tool-installer.nvim',
-         -- mason-tool-installer:
-        -- - Installs LSPs, linters, formatters, etc. by their Mason package name.
-        -- - We use it to ensure all desired tools are present.
-        -- - The `ensure_installed` list works with mason-lspconfig to resolve LSP names like "lua_ls".
-        {'j-hui/fidget.nvim', opts = {}},
-        -- Allows extra capabilities provided by nvim-cmp
-        'hrsh7th/cmp-nvim-lsp'
-    },
-    
-    -- config = function()
+        { 
+            'mason-org/mason.nvim',
+             config = function()
+                    require("mason").setup()
+             end
+        },
+        {
+            'mason-org/mason-lspconfig.nvim',
 
-    -- end
+            config = function()
+                require("mason-lspconfig").setup({
+                    ensure_installed = { "lua_ls" }
+                })
+            end
+        },
+        {
+            'nvim-telescope/telescope-ui-select.nvim',
+            config = function()
+                require("telescope").setup {
+                    extensions = {
+                        ["ui-select"] = {
+                            require("telescope.themes").get_dropdown {
+                            
+                            }
+                        }
+                    }
+                }
+                require("telescope").load_extension("ui-select")
+            end
+        },
+    },
+    config = function()
+        local lspconfig = require("lspconfig")
+        lspconfig.lua_ls.setup({})
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+        vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
 }
