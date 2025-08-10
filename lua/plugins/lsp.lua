b1635ba3f8334631e1ca075064513696cdb7a36b
SS56Bo <sohamsarkar560@gmail.com>
2025-08-10 13:20:15 +0530

    LSP updates: Added fidget.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,6 +8,16 @@ return {
              end
         },
         {
+            "j-hui/fidget.nvim",
+            opts = {
+                notification = {
+                    window = {
+                      winblend = 0, -- Background color opacity in the notification window
+                    },
+                },
+            },
+          }
+        {
             'mason-org/mason-lspconfig.nvim',
 
             config = function()
